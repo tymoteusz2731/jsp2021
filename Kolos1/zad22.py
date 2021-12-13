4b45393d776c13017c00cfa3eeb896b282b4092b
Tymoteusz Górski <tymoteusz2731@wp.pl>
2021-12-13 10:21:35 +0100

    lista 6

--- Kolos1/zad22.py
+++
@@ -1,9 +0,0 @@
-tablica=[]
-dzwignia = True
-
-while dzwignia == True:
-    a = input("WprowadŸ liczbê, zakoñcz wpisuj¹c 'end': ")
-    if a == 'end':
-        dzwignia = False
-    else:
-        tablica.append(a)
--- Kolos1/zad32.py
+++
@@ -1,6 +0,0 @@
-﻿n = int(input('Podaj n: '))
-
-for i in range(n):
-    print("*"*(n-i))
-for i in range(2,n+1):
-    print("*"*(i))
